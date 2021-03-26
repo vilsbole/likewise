eb38cb8bcc09490b4f839b8ff6ce76a8a57a7801
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2021-03-26 11:52:56 +0100

    Add theme-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
         allExtensions: true, // defaults to false
       },
     },
-    "gatsby-plugin-eslint",
+    'gatsby-plugin-eslint',
+    'gatsby-plugin-theme-ui',
   ],
 }
